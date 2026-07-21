582288f3b5c5700f8584f4ebf161c80e085f1654
Gustavo de Souza Santos <gustavossantos1670@gmail.com>
2026-07-20 21:51:21 -0300

    Listagem de produtos implementada

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -8,11 +8,13 @@
  * @author Adm
  */
 
+
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class ProdutosDAO {
@@ -24,19 +26,67 @@ public class ProdutosDAO {
     
     public void cadastrarProduto (ProdutosDTO produto){
         
+        String sql = "INSERT INTO produtos(nome, valor, status) VALUES (?, ?, ?)";
+
+    conn = new conectaDAO().connectDB();
+
+    try {
+
+        prep = conn.prepareStatement(sql);
+
+        prep.setString(1, produto.getNome());
+        prep.setInt(2, produto.getValor());
+        prep.setString(3, produto.getStatus());
+
+        prep.executeUpdate();
+
+        JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
+
+    } catch (Exception e) {
+
+        JOptionPane.showMessageDialog(null, "Erro ao cadastrar: " + e.getMessage());
+
+    }
         
-        //conn = new conectaDAO().connectDB();
-        
-        
+      
     }
     
-    public ArrayList<ProdutosDTO> listarProdutos(){
+    public List<ProdutosDTO> listarProdutos(){
         
-        return listagem;
+ 
+
+    List<ProdutosDTO> lista = new ArrayList<>();
+
+    String sql = "SELECT * FROM produtos";
+
+    try {
+        conectaDAO conexao = new conectaDAO();
+        Connection conn = conexao.connectDB();
+        PreparedStatement pstm = conn.prepareStatement(sql);
+        ResultSet rs = pstm.executeQuery();
+
+        while (rs.next()) {
+
+            ProdutosDTO obj = new ProdutosDTO();
+
+            obj.setId(rs.getInt("id"));
+            obj.setNome(rs.getString("nome"));
+            obj.setValor(rs.getInt("valor"));
+            obj.setStatus(rs.getString("status"));
+
+            lista.add(obj);
+        }
+
+    } catch (Exception e) {
+        e.printStackTrace();
     }
-    
-    
-    
-        
+
+    return lista;
 }
+    }
+    
+    
+    
+        
+
 
